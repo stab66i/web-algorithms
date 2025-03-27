18e821e3041121db3c4292557219a04973ec9fde
stab66i <stabbingmepls@gmail.com>
2025-03-27 19:45:00 +0700

    adding points and clearing the canvas

--- algorithms/antAlgorithm/script.js
+++ algorithms/antAlgorithm/script.js
@@ -3,3 +3,59 @@ let canvas = document.getElementById('canvas')
 
 canvas.width = 500;
 canvas.height = 500;
+
+let backgroundColor = "#598D66";
+canvas.style.background = backgroundColor;
+
+//---------- drawing ----------
+
+let radiusPoint = 10;
+
+
+function drawPoint(x, y) {
+    ctx.fillStyle = 'white';
+
+    ctx.beginPath();
+    ctx.arc(x, y, radiusPoint, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function refresh() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//-----------algorithm----------
+
+
+
+
+
+
+
+//----------- main -------------
+let points = [];
+
+
+
+//------------events-----------
+
+document.addEventListener("DOMContentLoaded", () => {
+    refresh();
+});
+
+document.getElementById('clear').onclick = clear;
+
+function clear() {
+    refresh();
+    points = [];
+}
+
+canvas.addEventListener('click', function(e) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    drawPoint(x, y);
+
+    points.push({x, y});
+});
+
