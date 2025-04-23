7889af0aa01971b46739838edd610645fae22456
stab66i <stabbingmepls@gmail.com>
2025-04-23 21:28:10 +0700

    made the title page and hid it by clicking the button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+function startApp() {
+    const intro = document.getElementById('intro');
+    const swiper = document.getElementById('swiper');
+
+    gsap.to(intro, {
+        opacity: 0,
+        duration: 0.8,
+        onComplete: () => {
+            intro.style.display = 'none';
+
+            swiper.style.display = 'block';
+            gsap.fromTo(swiper, 
+                { opacity: 0 }, 
+                { opacity: 1, duration: 1 }
+            );
+        }
+    });
+}
 const video = document.querySelector('.video-background');
 
 const swiperText = new Swiper('.swiper',{
